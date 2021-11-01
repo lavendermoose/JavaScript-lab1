568e564d326345d3343b7424788e113641701a82
danak <dana.killeen@gmail.com>
2021-11-01 19:38:35 -0400

    lab commit

--- lab.js
+++ lab.js
@@ -1,11 +1,11 @@
-let name = 'Mitch Cuckovich';
-let age = 24;
-let birthday = 'Jauary 24';
-let pineapplePizza = true;
+const name = 'Mitch Cuckovich';
+const age = 24;
+const birthday = 'Jauary 24';
+const pineapplePizza = true;
 
-let lifeEvents = ['I was born in Troy, Michigan.', 'I went to Hope College', 'I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event', 'I am a graduate of a Grand Circus bootcamp']
+const lifeEvents = ['I was born in Troy, Michigan.', 'I went to Hope College', 'I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event', 'I am a graduate of a Grand Circus bootcamp']
 
-if (pineapplePizza === true) {
+if (pineapplePizza) {
   console.log(`My name is ${name} and I like pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}`);
 } 
 else {
@@ -13,22 +13,22 @@ else {
 }
 
 for (let i = 0; i < lifeEvents.length; i++)  {
-  console.log()
+  console.log(lifeEvents[i];)
 }
 
 let counter = 0
 while (true) {
-  console.log(counter);
-  counter++;
 
-  const randomNumber = Math.floor(Math.random() * 10) + 1 
-  console.log(randomNumber);
+  let randomNumber = Math.floor(Math.random() * 10 - 1 + 1) + 1;
 
   if (randomNumber !==5) {
-    console.log('Random number is not equal to five');
+    counter++;
+    console.log(`${randomNumber} !==5`);
     }
+
   else (randomNumber ===5); {
-    console.log('Random number is equal to five');
+    counter++;
+    console.log(`5 === 5. It took ${counter} interations to randomly generate 5`);
     break;
   }
  }
